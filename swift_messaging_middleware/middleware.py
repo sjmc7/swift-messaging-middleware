9b99338db032ef26f294f8719521210eeec608cb
Steve McLellan <steven.j.mclellan@gmail.com>
2016-01-28 23:02:24 -0600

    Handles COPY operations

--- swift_messaging_middleware/middleware.py
+++ swift_messaging_middleware/middleware.py
@@ -74,14 +74,18 @@ class OsloMessagingContext(wsgi.WSGIContext):
         method = request.method
         if method not in ('POST', 'PUT', 'COPY', 'DELETE'):
             return self.app(env, start_response)
+
+        response = self._app_call(env)
+        status_code = self._get_status_int()
+
         try:
             ver, account, container, obj = request.split_path(
                 2, 4, rest_with_last=True)
         except ValueError:
-            return self.app(env, start_response)
-
-        response = self._app_call(env)
-        status_code = self._get_status_int()
+            start_response(self._response_status,
+                       self._response_headers,
+                       self._response_exc_info)
+            return response
 
         event_methods = {
             'DELETE': 'delete',
@@ -97,11 +101,12 @@ class OsloMessagingContext(wsgi.WSGIContext):
 
         if status_code in (200, 201, 202, 204):
             request_headers = request.headers
-            print dir(request)
-            print request.__dict__
-            print request.headers.items()
-
-            print(self._response_headers)
+            #print ver, account, container, obj
+            #print dir(request)
+            #print request.__dict__
+            #print request.environ['PATH_INFO']
+            #print request.headers.items()
+            #print(self._response_headers)
 
             payload = self._get_request_auth_info(request_headers)
             payload['account'] = account
@@ -112,6 +117,21 @@ class OsloMessagingContext(wsgi.WSGIContext):
 
 
             if method != 'DELETE':
+                copy_from = request_headers.get('X-Copy-From')
+                if copy_from:
+                    # Copies are turned into PUTs with an X-Copy-From in the object middleware
+                    print "Detected copy %s" % copy_from
+                    event_type = event_methods['COPY']
+                    if copy_from[0] == '/':
+                        copy_from = copy_from[1:]
+                    copy_from_container, copy_from_object = copy_from.split('/', 1)
+
+                    payload['copy_from_container'] = copy_from_container
+                    payload['copy_from_object'] = copy_from_object
+
+                    if request_headers.get('X-Fresh-Metadata', None):
+                        payload['copy-fresh-metadata'] = bool(request_headers.get('X-Fresh-Metadata'))
+
                 payload.update(self._get_account_metadata(request_headers, self._response_headers))
                 if container:
                     payload.update(self._get_container_metadata(request_headers, self._response_headers))
@@ -124,7 +144,7 @@ class OsloMessagingContext(wsgi.WSGIContext):
                     modified_datetime = datetime.fromtimestamp(float(modified_timestamp))
                     payload['updated_at'] = modified_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')
 
-                if method == 'PUT':
+                if method in ('PUT', 'COPY'):
                     payload['content_length'] = env['CONTENT_LENGTH']
 
             self._notifier.info({}, event_type, payload)
